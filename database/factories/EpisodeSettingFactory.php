6e67bab39a0afb56a1db8b7088935ffd2559a3e4
Ryan Gurnick <ryangurnick@gmail.com>
2020-11-13 20:14:23 -0800

    adding factories for many tables
    adding in placeholder factories.

---
+++ database/factories/EpisodeSettingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\EpisodeSetting;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EpisodeSettingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = EpisodeSetting::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/SpotifyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Spotify;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SpotifyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Spotify::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
