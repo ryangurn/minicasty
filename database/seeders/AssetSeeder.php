5dee89f99be150e31e6b47b62aef81b57d8b4e3a
Ryan Gurnick <ryangurnick@gmail.com>
2020-11-26 12:41:25 -0800

    Adding images to seeder

--- database/seeders/AssetSeeder.php
+++ database/seeders/AssetSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Asset;
+use App\Models\Setting;
 use Illuminate\Database\Seeder;
 
 class AssetSeeder extends Seeder
@@ -13,6 +15,11 @@ class AssetSeeder extends Seeder
      */
     public function run()
     {
-        //
+        # todo: add image into table and assoc with settings
+        $podcast_image = Setting::where('key', '=', 'podcast-image')->first();
+
+        $asset = Asset::create(['guid' => $podcast_image->guid, 'image' => true, 'audio' => false, 'path' => '20dbc889d70e623bafcc4b826884fceb.jpeg', 'accessible' => true]);
+        $podcast_image->value = Asset::where('guid', '=', $podcast_image->guid)->first()->guid;   # set value to asset guid
+        $podcast_image->save(); # save
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(SettingSeeder::class);
 
+        $this->call(AssetSeeder::class);
+
     }
 }
