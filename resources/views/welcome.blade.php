6e704f2de0955b4dc8bb0b39e46bab09a31d5a6f
Ryan Gurnick <ryangurnick@gmail.com>
2020-12-07 17:22:30 -0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,10 @@
 			<h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
 				mini[casty]
 				<br>
-				<span class="text-indigo-600">time to get podding!</span>
+				<div>
+					<span class="block text-indigo-600">time to get podding!</span>
+					<span class="block text-indigo-900 text-xs">Please make sure to use firefox or chrome, safari has some issues.</span>
+				</div>
 			</h2>
 			<div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
 				<div class="inline-flex rounded-md shadow">
