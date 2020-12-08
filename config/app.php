c5727ff43f5d1ca334468a1a9439926d394ab3a2
Ryan Gurnick <ryangurnick@gmail.com>
2020-12-07 17:09:22 -0800

    adding parsedown

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Parsedown\Providers\ParsedownServiceProvider::class,
 
         /*
          * Application Service Providers...
